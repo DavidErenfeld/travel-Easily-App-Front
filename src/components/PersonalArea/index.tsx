c9a1090f9febe5d4309d040ac836ece319bd3f35
David Erenfeld <1020dudu@gmail.com>
2024-11-01 02:36:08 +0200

    Add important untracked frontend files

---
+++ src/components/PersonalArea/index.tsx
@@ -0,0 +1,193 @@
+import { useEffect, useRef, useState } from "react";
+import { deleteUser, updateUser } from "../../services/usersService";
+import {
+  uploadPhoto,
+  deletePhotoFromCloudinary,
+} from "../../services/fileService";
+import CloseIcon from "../UIComponents/Icons/Close";
+import LoadingDots from "../UIComponents/Loader";
+import "./style.css";
+import { useNavigate } from "react-router-dom";
+import tripsService from "../../services/tripsService";
+
+function PersonalArea() {
+  const imgRef = useRef<HTMLInputElement>(null);
+  const userName = localStorage.getItem("userName") || "";
+  const [imgFile, setImgFile] = useState<File | null>(null);
+  const [imgSrc, setImgSrc] = useState(localStorage.getItem("imgUrl"));
+  const [isButtonClicked, setButtonClicked] = useState(false);
+  const loggedUserId = localStorage.getItem("loggedUserId");
+  const [loading, setLoading] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const navigate = useNavigate();
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
+
+  let imgUrl = localStorage.getItem("imgUrl") || "";
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setButtonClicked(true);
+      setImgFile(e.target.files[0]);
+      setImgSrc(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+
+  const confirmDeleteUser = () => {
+    setShowDeletePopup(true);
+  };
+
+  const handleDeleteUser = async () => {
+    const userId = localStorage.getItem("loggedUserId");
+
+    if (!userId) {
+      console.log("No user logged in");
+      return;
+    }
+
+    try {
+      setIsDeleting(true);
+
+      const userTrips: any = await tripsService.getByOwnerId(userId);
+
+      for (const trip of userTrips) {
+        for (const image of trip.tripPhotos || []) {
+          await deletePhotoFromCloudinary(image);
+        }
+      }
+
+      await deleteUser(userId);
+      console.log("User deleted and logged out successfully");
+      navigate("/");
+    } catch (error) {
+      console.log("Error deleting user:", error);
+    } finally {
+      await deleteUser(userId);
+      setIsDeleting(false);
+    }
+  };
+
+  const onClickSave = async () => {
+    try {
+      setLoading(true);
+      if (imgFile) {
+        if (imgUrl) {
+          await deletePhotoFromCloudinary(imgUrl);
+          console.log(`Previous image ${imgUrl} deleted successfully.`);
+        }
+
+        imgUrl = (await handleUploadImage(imgFile)) || "";
+      }
+
+      const response = await updateUser(loggedUserId || "", { imgUrl: imgUrl });
+      console.log(response);
+      localStorage.setItem("imgUrl", imgUrl);
+      setButtonClicked(false);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleUploadImage = async (imgFile: File) => {
+    try {
+      setLoading(true);
+      setButtonClicked(false);
+      const uploadedUrl = await uploadPhoto(imgFile);
+      console.log(`Image uploaded successfully: ${uploadedUrl}`);
+      return uploadedUrl;
+    } catch (error) {
+      console.error("Upload failed:", error);
+      alert("Failed to upload image.");
+      return null;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (imgSrc) URL.revokeObjectURL(imgSrc);
+    };
+  }, [imgSrc]);
+
+  useEffect(() => {
+    if (showDeletePopup) {
+      document.body.classList.add("popup-open");
+    } else {
+      document.body.classList.remove("popup-open");
+    }
+  }, [showDeletePopup]);
+
+  return (
+    <>
+      <section className="personal-area-section form-container flex-center-column-large-gap">
+        <input
+          type="file"
+          name="img"
+          ref={imgRef}
+          style={{ display: "none" }}
+          onChange={handleChange}
+        />
+        <div className="form-close-icon">
+          <CloseIcon color="#fff" />
+        </div>
+        <p className="form-title">Personal Area</p>
+
+        {imgSrc && (
+          <img className="register-img " src={imgSrc} alt="img profile" />
+        )}
+        <h1 className="profile-name">{userName}</h1>
+
+        {loading ? (
+          <div className="loader-section">
+            <LoadingDots />
+          </div>
+        ) : (
+          <div className="btn-container-gap-m ">
+            <button onClick={() => imgRef.current?.click()} className="btn-m">
+              Edit
+            </button>
+
+            {isButtonClicked && (
+              <button onClick={onClickSave} className="btn-m">
+                Save
+              </button>
+            )}
+          </div>
+        )}
+
+        {isDeleting ? (
+          <div className="loader-section">
+            <LoadingDots />
+          </div>
+        ) : (
+          <button className="btn-m delete-btn" onClick={confirmDeleteUser}>
+            Delete account
+          </button>
+        )}
+      </section>
+
+      {showDeletePopup && !isDeleting && (
+        <div className="popup-overlay">
+          <div className="pop-up">
+            <p>Are you sure you want to delete your account?</p>
+            <div className="pop-up-buttons">
+              <button className="btn-m" onClick={handleDeleteUser}>
+                Delete
+              </button>
+              <button
+                className="btn-m"
+                onClick={() => setShowDeletePopup(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PersonalArea;
---
+++ src/components/TripComponents/CreateTrip/index.tsx
@@ -0,0 +1,199 @@
+import { useState, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import tripsService, { ITrips } from "../../../services/tripsService";
+import Header from "../../Header";
+import AddImgs from "../../UIComponents/Icons/AddImage";
+import ImageCarousel from "../../UIComponents/ImageCarousel";
+import LoadingDots from "../../UIComponents/Loader";
+import SuccessMessage from "../../UIComponents/SuccessMessage";
+import useSocket from "../../../Hooks/useSocket";
+import useImageUpload from "../../../Hooks/useImageUpload";
+import "./style.css";
+
+interface TripDay {
+  dayNum: number;
+  description: string;
+}
+
+const CreateTrip: React.FC = () => {
+  const location = useLocation();
+  const { numberOfDays, selectedGroupType, selectedTripType, selectedCountry } =
+    location.state || {};
+
+  // State variables
+  const [currentDayIndex, setCurrentDayIndex] = useState(0);
+  const [dayEdits, setDayEdits] = useState<TripDay[]>(
+    Array.from({ length: numberOfDays }, (_, index) => ({
+      dayNum: index + 1,
+      description: "",
+    }))
+  );
+
+  const {
+    images,
+    handleImageChange,
+    deleteImage,
+    uploadImages,
+    openImageSelector,
+  } = useImageUpload([]);
+  const [errorMessages, setErrorMessages] = useState<string[]>([]);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  // Refs and Navigation
+  const imageRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
+  // useSocket Hook for sending new trip event
+  const { send } = useSocket();
+
+  // Function to handle description change for each day
+  const handleDescriptionChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const updatedDays = dayEdits.map((day, index) =>
+      index === currentDayIndex
+        ? { ...day, description: event.target.value }
+        : day
+    );
+    setDayEdits(updatedDays);
+
+    // Clear error for the current day if there's an input
+    const updatedErrors = [...errorMessages];
+    updatedErrors[currentDayIndex] = "";
+    setErrorMessages(updatedErrors);
+  };
+
+  // Handle trip submission
+  const handleSubmit = async () => {
+    // Validate each day's description
+    const errors: string[] = [];
+    dayEdits.forEach((day, index) => {
+      if (!day.description.trim()) {
+        errors[
+          index
+        ] = `Day ${day.dayNum} is empty. Please fill in the description.`;
+      }
+    });
+
+    if (errors.length > 0) {
+      setErrorMessages(errors);
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    // Prepare trip data
+    const tripData = dayEdits.map((day) => day.description);
+    const tripPhotos = await uploadImages();
+
+    const trip: ITrips = {
+      userName: localStorage.getItem("userName") || undefined,
+      typeTraveler: selectedGroupType,
+      country: selectedCountry,
+      typeTrip: selectedTripType,
+      numOfDays: numberOfDays,
+      tripDescription: tripData,
+      numOfComments: 0,
+      numOfLikes: 0,
+      tripPhotos,
+      comments: [],
+    };
+
+    try {
+      const savedTrip = await tripsService.postTrip(trip);
+      send("newTrip", savedTrip); // Sending new trip event to server
+      setShowSuccessMessage(true);
+    } catch (error) {
+      console.error("Failed to save the trip:", error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      {showSuccessMessage && (
+        <SuccessMessage
+          message="Trip saved successfully!"
+          onAnimationEnd={() => {
+            setShowSuccessMessage(false);
+            navigate("/");
+          }}
+        />
+      )}
+      <section
+        className={`create-trip-section update-trip-section flex-stretch-column-gap section ${
+          isSubmitting ? "blurred-background" : ""
+        }`}
+      >
+        <div className="update-trip-container">
+          <div className="update-details">
+            <p className="day-name">Day {dayEdits[currentDayIndex].dayNum}</p>
+          </div>
+          <textarea
+            className="update-trip-description"
+            value={dayEdits[currentDayIndex].description}
+            onChange={handleDescriptionChange}
+            placeholder={`Share with us what you did on day ${dayEdits[currentDayIndex].dayNum}`}
+          />
+          {errorMessages[currentDayIndex] && (
+            <p className="error-message">{errorMessages[currentDayIndex]}</p>
+          )}
+
+          <div
+            className="add-image-icon"
+            onClick={() => imageRef.current?.click()}
+          >
+            <AddImgs />
+          </div>
+          <input
+            type="file"
+            multiple
+            ref={imageRef}
+            style={{ display: "none" }}
+            onChange={handleImageChange}
+          />
+
+          <button
+            className="scroll-button left"
+            onClick={() => setCurrentDayIndex(currentDayIndex - 1)}
+            disabled={currentDayIndex === 0}
+          >
+            ‹
+          </button>
+          <button
+            className="scroll-button right"
+            onClick={() => setCurrentDayIndex(currentDayIndex + 1)}
+            disabled={currentDayIndex === dayEdits.length - 1}
+          >
+            ›
+          </button>
+        </div>
+
+        {currentDayIndex === dayEdits.length - 1 && (
+          <button className="btn-l submit-trip-btn" onClick={handleSubmit}>
+            Submit
+          </button>
+        )}
+
+        {images.length > 0 && (
+          <ImageCarousel
+            deleteImage={deleteImage}
+            images={images}
+            showDeleteButton={true}
+          />
+        )}
+      </section>
+
+      {isSubmitting && (
+        <div className="loading-overlay">
+          <LoadingDots />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CreateTrip;
