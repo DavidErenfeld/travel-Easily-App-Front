8c92c41d16215549cf9a45e7d76c60ed707ac30e
David Erenfeld <1020dudu@gmail.com>
2024-11-05 04:04:29 +0200

    Adding logs to socket functions

--- src/Hooks/useSocket.tsx
+++ src/Hooks/useSocket.tsx
@@ -5,13 +5,21 @@ import io from "socket.io-client";
 const socket = io("https://evening-bayou-77034-176dc93fb1e1.herokuapp.com/", {
   transports: ["websocket"], // שימוש רק ב-WebSocket
 });
+
+console.log("Socket initialized"); // לוג בעת אתחול החיבור
+
 const useSocket = (event?: string, onEvent?: (data: any) => void) => {
   // האזנה לאירועים
   useEffect(() => {
     if (event && onEvent) {
-      socket.on(event, onEvent);
+      console.log(`Listening to event: ${event}`); // לוג בעת התחלת האזנה לאירוע ספציפי
+      socket.on(event, (data) => {
+        console.log(`Received data for event ${event}:`, data); // לוג עם הנתונים המתקבלים
+        onEvent(data);
+      });
 
       return () => {
+        console.log(`Stopped listening to event: ${event}`); // לוג בעת הפסקת האזנה לאירוע
         socket.off(event);
       };
     }
@@ -19,6 +27,7 @@ const useSocket = (event?: string, onEvent?: (data: any) => void) => {
 
   // פונקציה לשליחת אירועים
   const send = useCallback((event: string, data: any) => {
+    console.log(`Sending event: ${event} with data:`, data); // לוג בעת שליחת אירוע עם הנתונים
     socket.emit(event, data);
   }, []);
 
