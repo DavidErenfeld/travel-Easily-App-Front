1e82ddc21ea05afb80c2f5a03b954e0ccd434c7a
David Erenfeld <1020dudu@gmail.com>
2024-11-27 11:52:10 +0200

    Updated Vite config for code splitting and optimized build output

--- src/components/Home/HeroSection/index.tsx
+++ src/components/Home/HeroSection/index.tsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import i18n from "../../../i18n";
+import { Helmet } from "react-helmet";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import "./style.css";
 
 const HeroSection = () => {
@@ -8,28 +11,35 @@ const HeroSection = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="hero-section">
-      <div className={`description-container`}>
-        <h1 className={`${i18n.language === "he" ? "he-title" : ""} `}>
-          {t("heroTitle")}
-        </h1>
-        <p>{t("heroDescription")}</p>
-      </div>
-      <div className="earth-container">
-        <img
-          src="/images/Earth.webp"
-          alt={t("heroAltImage")}
-          className="earth-image"
-        />
-      </div>
+    <>
+      <Helmet>
+        <link rel="preload" as="image" href="/images/Earth.webp" />
+      </Helmet>
 
-      <button
-        onClick={() => navigate("/secontHomPage")}
-        className="btn-cta-exl"
-      >
-        {t("heroCTA")}
-      </button>
-    </section>
+      <section className="hero-section">
+        <div className={`description-container`}>
+          <h1 className={`${i18n.language === "he" ? "he-title" : ""} `}>
+            {t("heroTitle")}
+          </h1>
+          <p>{t("heroDescription")}</p>
+        </div>
+        <div className="earth-container">
+          <LazyLoadImage
+            src="/images/Earth.webp"
+            alt={t("heroAltImage")}
+            className="earth-image"
+            effect="blur"
+          />
+        </div>
+
+        <button
+          onClick={() => navigate("/secontHomPage")}
+          className="btn-cta-exl"
+        >
+          {t("heroCTA")}
+        </button>
+      </section>
+    </>
   );
 };
 
